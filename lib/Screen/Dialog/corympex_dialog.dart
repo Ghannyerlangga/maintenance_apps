e2f58f2b7a76934571e29d6813ae454bbde55ee4
Ghanny Erlangga <ghannyerlangga@gmail.com>
2020-07-13 11:53:20 +0700

    Simple Code

---
+++ lib/Screen/Dialog/corympex_dialog.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:maintenance_apps/Screen/Checklist/annual.dart';
+import 'package:maintenance_apps/Screen/Checklist/daily.dart';
+import 'package:maintenance_apps/Screen/Checklist/monthly.dart';
+import 'package:maintenance_apps/Screen/Checklist/plasma.dart';
+
+class CorympexDialog extends StatefulWidget {
+  final String value;
+  final String hasil;
+  CorympexDialog({this.value, this.hasil});
+  @override
+  _CorympexDialogState createState() => _CorympexDialogState();
+}
+
+class _CorympexDialogState extends State<CorympexDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Container(
+        height: 300,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "FICEP",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 120.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Daily(
+                                  value: widget.value, hasil: widget.hasil),
+                            ));
+                      },
+                      child: Text(
+                        "Daily",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: const Color(0xFF1BC0C5),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 120.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Monthly(
+                                  value: widget.value, hasil: widget.hasil),
+                            ));
+                      },
+                      child: Text(
+                        "Monthly",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: const Color(0xFF1BC0C5),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 120.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Annual(
+                                  value: widget.value, hasil: widget.hasil),
+                            ));
+                      },
+                      child: Text(
+                        "Semi-Annual",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: const Color(0xFF1BC0C5),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "FICEP",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 120.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Plasma(
+                                  value: widget.value, hasil: widget.hasil),
+                            ));
+                      },
+                      child: Text(
+                        "Daily",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: const Color(0xFF1BC0C5),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
